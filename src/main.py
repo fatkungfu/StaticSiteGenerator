398394d56b5931c801773cdda5afa71dec80c498
Eric Bourcier-Walsh <fatkungfu@gmail.com>
2025-04-24 22:54:52 -0400

    add Unit tests for TextNode

--- src/main.py
+++ src/main.py
@@ -2,10 +2,8 @@ from textnode import TextNode, TextType
 
 
 def main():
-    # Create a sample link TextNode
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(node)
 
 
-if __name__ == "__main__":
-    main()
+main()
